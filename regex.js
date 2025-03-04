67da9850afcd3308802b228fff774b2f62f56fd4
abhishek <rajabhishek7654@gmail.com>
2025-03-04 10:53:05 +0530

    [Abhishek] add. JS Regex Handling Pin and Email Validation

--- regex.js
+++ regex.js
@@ -1,8 +1,34 @@
-function validateEmail_UC5(email) {
-        const pattern = /^[a-zA-Z0-9._+-]+@[a-zA-Z]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2})?$/;
-        return pattern.test(email);
+function validatePin(pin) {
+        const pinPattern = /^(?![A-Za-z])\d{3}\s?\d{3}(?![A-Za-z])$/;
+        if (pinPattern.test(pin)) {
+            console.log(`Valid PIN: ${pin}`);
+            return true;
+        } else {
+            console.error(`Invalid PIN: ${pin}`);
+            return false;
+        }
     }
     
-    console.log(validateEmail_UC5("abc.xyz@bridgelabz.co.in")); 
-    console.log(validateEmail_UC5("abc@gmail.com.1a"));        
+    function validateEmail(email) {
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (emailPattern.test(email)) {
+            console.log(`Valid Email: ${email}`);
+            return true;
+        } else {
+            console.error(`Invalid Email: ${email}`);
+            return false;
+        }
+    }
+    
+    console.log("Testing PIN validation:");
+    validatePin("400088");    
+    validatePin("400 088");   
+    validatePin("A400088");   
+    validatePin("400088B");   
+    
+    console.log("\nTesting Email validation:");
+    validateEmail("abc.xyz@bridgelabz.co.in"); 
+    validateEmail("abc@bridgelabz.co");        
+    validateEmail("abc@.com.my");              
+    validateEmail("abc@abc@gmail.com");        
     
